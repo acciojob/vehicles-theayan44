e4a6291617462246501038b8c5890ef8c2167469
ayan <theayan44@gmail.com>
2023-06-10 17:12:26 +0530

    main

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,9 +4,9 @@ public class Boat implements WaterVehicle {
     private String name;
     private int capacity;
 
-    public Boat() {
-        name = "";
-        capacity = 0;
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
     }
 
     public String getVehicleName(){
